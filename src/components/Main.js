f06d14278c73148d43abff5af304f4ee8bff98c2
Fetra01 <fetra.andria@outlook.fr>
2020-11-05 14:38:16 +0100

    Création et finition d'un application authentification avec firebase, facebook, gmail et Email

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import firebase from '../utils/firebaseConfig';
+
+const Main = () => {
+    return (
+        <main>
+            <nav>
+                <h1>React Crud</h1>
+                <h4>Bonjour {firebase.auth().currentUser.displayName}</h4>
+                <div onClick={() => firebase.auth().signOut()}>Se déconnecter</div>
+            </nav>
+        </main>
+    );
+};
+
+export default Main;
